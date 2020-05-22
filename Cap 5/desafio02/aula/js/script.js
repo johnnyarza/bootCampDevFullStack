d727dd361053c2169d9e19353859e37bec2a7495
Johny Arza <johnnyarza@gmail.com>
2020-05-22 14:04:09 -0300

    final desafio

--- Cap 5/desafio02/aula/js/script.js
+++ Cap 5/desafio02/aula/js/script.js
@@ -36,7 +36,13 @@ async function fetchCountries() {
   const json = await res.json();
   allCountries = json.map((country) => {
     const { numericCode, translations, population, flag } = country;
-    return { id: numericCode, name: translations.pt, population, flag };
+    return {
+      id: numericCode,
+      name: translations.pt,
+      population,
+      formattedPopulation: formatNumber(population),
+      flag,
+    };
   });
   render();
 }
@@ -52,23 +58,113 @@ function renderCountryList() {
   let countriesHTML = '<div>';
 
   allCountries.forEach((country) => {
-    const { name, flag, id, population } = country;
+    const { name, flag, id, population, formattedPopulation } = country;
     const countryHTML = `
     <div class='country'>
-
       <div>
+        <a id="${id}" class="waves-effect waves-light btn">+</a>
       </div>
 
       <div>
+        <img src="${flag}" alt="${name}">
       </div>
       
       <div>
+        <ul>
+          <li>${name}</li>
+          <li>${formattedPopulation}</li>
+        </ul>
       </div>
 
     </div>
     `;
+    countriesHTML += countryHTML;
+  });
+  countriesHTML += '</div>';
+  tabCountries.innerHTML = countriesHTML;
+}
+function renderFavorites() {
+  let favoritesHTML = '<div>';
+
+  favoritesCountries.forEach((country) => {
+    const { name, flag, id, population, formattedPopulation } = country;
+
+    const favoriteCountryHTML = `
+      <div class='country'>
+        <div>
+          <a id="${id}" class="waves-effect waves-light btn red darken-4">-</a>
+        </div>
+
+        <div>
+          <img src="${flag}" alt="${name}">
+        </div>
+        
+        <div>
+          <ul>
+            <li>${name}</li>
+            <li>${formattedPopulation}</li>
+          </ul>
+        </div>
+
+      </div>
+      `;
+    favoritesHTML += favoriteCountryHTML;
+  });
+
+  favoritesHTML += '</div>';
+  tabFavorites.innerHTML = favoritesHTML;
+}
+
+function renderSummary() {
+  countCountries.textContent = allCountries.length;
+  countFavorites.textContent = favoritesCountries.length;
+
+  const totalPopulation = allCountries.reduce((accumulator, current) => {
+    return accumulator + current.population;
+  }, 0);
+  const totalFavorites = favoritesCountries.reduce((accumulator, current) => {
+    return accumulator + current.population;
+  }, 0);
+
+  totalPopulationList.textContent = formatNumber(totalPopulation);
+  totalPopulationFavorites.textContent = formatNumber(totalFavorites);
+}
+
+function handleCountryButtons() {
+  const countryButtons = Array.from(tabCountries.querySelectorAll('.btn'));
+  const favoriteButtons = Array.from(tabFavorites.querySelectorAll('.btn'));
+
+  countryButtons.forEach((button) => {
+    button.addEventListener('click', () => addToFavorites(button.id));
+  });
+
+  favoriteButtons.forEach((button) => {
+    button.addEventListener('click', () => removeFromFavorites(button.id));
   });
 }
-function renderFavorites() {}
-function renderSummary() {}
-function handleCountryButtons() {}
+
+function addToFavorites(id) {
+  const countryToAdd = allCountries.find((country) => country.id === id);
+  favoritesCountries = [...favoritesCountries, countryToAdd];
+  favoritesCountries.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  });
+  allCountries = allCountries.filter((country) => country.id !== id);
+  render();
+}
+
+function removeFromFavorites(id) {
+  const countryToRemove = favoritesCountries.find(
+    (country) => country.id === id
+  );
+  allCountries = [...allCountries, countryToRemove];
+  allCountries.sort((a, b) => a.name.localeCompare(b.name));
+  favoritesCountries = favoritesCountries.filter(
+    (country) => country.id !== id
+  );
+  render();
+}
+
+function formatNumber(number) {
+  return numberFormat.format(number);
+}
--- Desafio do módulo 1/js/script.js
+++ Desafio do módulo 1/js/script.js
@@ -1,26 +1,110 @@
 let users = [];
+let allUsers = [];
 let userList = null;
+let statisticsList = null;
+let userSummarizeTitle = null;
+let searchUserInput = null;
+let searchButton = null;
+let numberFormat = null;
+
 window.addEventListener('load', () => {
-  let searchButton = document.querySelector('#search-button');
+  searchButton = document.querySelector('#search-button');
   userList = document.querySelector('#user-list');
+  userSummarizeTitle = document.querySelector('#user-summarize-title');
+  searchUserInput = document.querySelector('#search-user-input');
+  statisticsList = document.querySelector('#statistics-list');
+
   searchButton.addEventListener('click', onSearchBtClick);
+  searchUserInput.addEventListener('input', searchInputAction);
+  numberFormat = Intl.NumberFormat('pt-BR');
+
+  fetchUsers();
 });
 
-function onSearchBtClick() {
-  fetchUsers();
+function searchInputAction() {
+  // $('#search-button')
+  //   .removeClass('waves-effect waves-light btn disabled')
+  //   .addClass('waves-effect waves-light btn');
+  if (
+    searchUserInput.value.length !== 0 &&
+    searchButton.classList.contains('disabled')
+  ) {
+    searchButton.classList.remove('disabled');
+  } else if (
+    searchUserInput.value.length === 0 &&
+    !searchButton.classList.contains('disabled')
+  ) {
+    searchButton.classList.add('disabled');
+  }
+}
+
+function onSearchBtClick(event) {
+  searchUsers();
+  render();
+}
+
+function searchUsers() {
+  users = filterUsers();
 }
 
 function render() {
+  setStatistics();
+  setUserResults();
+  setUserSummarizeTitle(users.length);
+  setUserResults();
+}
+
+function setStatistics() {
+  statisticsList.innerHTML = ``;
+  countMales();
+  countFemales();
+  let sum = sumAge();
+  ageMed(sum);
+}
+
+function countMales() {
+  let count = users.filter((user) => user.gender === 'male').length;
+
+  statisticsList.innerHTML += `
+  <div>Sexo masculino: <span>${count}</span></div>
+  `;
+}
+
+function countFemales() {
+  let count = users.filter((user) => user.gender === 'female').length;
+  statisticsList.innerHTML += `
+  <div>Sexo feminino: <span>${count}</span></div>
+  `;
+}
+
+function sumAge() {
+  const sum = users.reduce((accumulator, current) => {
+    return accumulator + current.age;
+  }, 0);
+  statisticsList.innerHTML += `
+  <div>Soma das idades: <span>${sum}</span></div>`;
+  return sum;
+}
+function ageMed(sum) {
+  let med = (sum / users.length).toFixed(2);
+  med = numberFormat.format(med);
+  statisticsList.innerHTML += `<div>Média das idades: <span>${med}</span></div`;
+}
+
+function setUserResults() {
   let usersHTML = '<div>';
+
   users.forEach((user) => {
     const { firstName, lastName, picture, age, gender } = user;
     const userHTML = `
-    <div>
-    <img src="${picture}" alt="${firstName}">
-    </div>
+    <div class = 'user-list z-depth-2'>
+      <div>
+        <img src="${picture}" alt="${firstName}" class="circle">
+      </div>
     
-    <div> 
-    ${firstName} ${lastName}, ${age} anos
+      <div> 
+        <h6>${firstName} ${lastName}, ${age} anos</h6>
+      </div>
     </div>
     `;
     usersHTML += userHTML;
@@ -29,12 +113,30 @@ function render() {
   userList.innerHTML = usersHTML;
 }
 
+function filterUsers() {
+  return allUsers.filter((user) => {
+    return (user.firstName + ' ' + user.lastName)
+      .toLowerCase()
+      .includes(searchUserInput.value.toLowerCase());
+  });
+}
+
+function setUserSummarizeTitle(length) {
+  if (length > 0) {
+    userSummarizeTitle.textContent = `${length} usuários encontrados`;
+  } else if (length === 1) {
+    userSummarizeTitle.textContent = `${length} usuário encontrado`;
+  } else if (length === 0) {
+    userSummarizeTitle.textContent = 'Nenhum usuário encontrado';
+  }
+}
+
 async function fetchUsers() {
   const res = await fetch(
     'https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo'
   );
   const data = await res.json();
-  users = data.results.map((user) => {
+  allUsers = data.results.map((user) => {
     const { name, picture, dob, gender } = user;
     return {
       firstName: name.first,
@@ -44,5 +146,4 @@ async function fetchUsers() {
       gender: gender,
     };
   });
-  render();
 }
